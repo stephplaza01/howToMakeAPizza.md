b9ac34c593ed2fc090a334dfacf27c8e20877f65
stephanydiaz01 <stephplaza0104@gmail.com>
2023-11-25 17:23:42 -0500

    delipizzas

---
+++ gitstephplaza.py
@@ -0,0 +1,7 @@
+#Prepare los ingredientes: necesitarás masa para pizza, salsa de tomate, queso mozzarella y los ingredientes de tu elección (como pepperoni, champiñones, pimientos, etc.).
+#Precalienta el horno a 200 grados Celsius.
+#Extiende la masa de pizza en una bandeja para hornear.
+#Aplique una capa uniforme de salsa de tomate sobre la masa.
+#Agrega una capa de queso mozzarella rallado.
+#Coloca los ingredientes de tu elección encima del queso.
+#Hornea la pizza en el horno precalentado durante unos 15-20 minutos,
